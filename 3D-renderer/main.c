835d9aaf679db792b74a724b9f35e6f36173cf28
Otso Kangas <rapulintu@gmail.com>
2025-04-20 19:25:47 +0300

    Gitignore

---
+++ 3D-renderer/main.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <SDL.h>
+
+bool is_running = false;
+bool is_fullscreen = false;
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+
+int window_width = 800;
+int window_height = 600;
+
+
+
+bool initialize_window(void) {
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return false;
+    }
+    if (is_fullscreen) {
+
+        SDL_DisplayMode display_mode;
+        SDL_GetCurrentDisplayMode(0, &display_mode);
+        window_width = display_mode.w;
+        window_height = display_mode.h;
+        window = SDL_CreateWindow(
+            NULL,
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            window_width,
+            window_height,
+            SDL_WINDOW_BORDERLESS
+        );
+        SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+    }
+
+
+    else {
+        window = SDL_CreateWindow(
+            NULL,
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            window_width,
+            window_height,
+            SDL_WINDOW_BORDERLESS
+        );
+    }
+    
+    if (!window) {
+        fprintf(stderr, "Error creating SDL window.\n");
+        return false;
+    }
+
+    
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL renderer.\n");
+        return false;
+    }
+
+    return true;
+}
+
+void setup(void) {
+
+    color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * window_width * window_height);
+
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
+   
+}
+
+void process_input(void) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+    case SDL_QUIT:
+        is_running = false;
+        break;
+    case SDL_KEYDOWN:
+        if (event.key.keysym.sym == SDLK_ESCAPE)
+            is_running = false;
+        break;
+    }
+}
+
+void update(void) {
+   
+}
+
+void render_color_buffer(void) {
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        (int)(window_width * sizeof(uint32_t))
+    );
+    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color) {
+    for (int y = 0; y < window_height; y++) {
+        for (int x = 0; x < window_width; x++) {
+            color_buffer[(window_width * y) + x] = color;
+        }
+    }
+}
+
+void draw_bg_grid() {
+
+    for (int y = 0; y < window_height; y ++) {
+        for (int x = 0; x < window_width; x++) {
+            if (x % 10 == 0 || y % 10 == 0) {
+                color_buffer[(window_width * y) + x] = 0x11111111;
+           }
+        }
+    }
+}
+
+void draw_rect(int top_left_x, int top_y, int width, int height, uint32_t color) {
+
+
+    int dy = height + top_y;
+    int dx = width + top_left_x;
+    for (int j = top_y; j < dy; j++) {
+        for (int i = top_left_x; i < dx; i++) {
+            color_buffer[(window_width * j) + i] = color;
+        }
+    }
+
+}
+
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    clear_color_buffer(0x00000000);
+
+    draw_bg_grid();
+    draw_rect(300, 200, 300, 100, 0xFFFF0000);
+    render_color_buffer();
+
+    SDL_RenderPresent(renderer);
+}
+
+
+
+void destroy_window(void) {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char **argv) {
+    is_running = initialize_window();
+
+    setup();
+
+   
+
+    while (is_running) {
+        process_input();
+        update();
+        render();
+    }
+
+    destroy_window();
+
+    return 0;
+}
