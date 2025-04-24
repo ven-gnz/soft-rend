e71c1c114ce8d7527890e532182bb04d866417d3
Otso Kangas <kangas.otso@gmail.com>
2025-04-24 20:41:37 +0300

    DDA line drawing

--- 3D-renderer/display.h
+++ 3D-renderer/display.h
@@ -27,5 +27,6 @@ void clear_color_buffer(uint32_t color);
 void draw_pixel(int x, int y, uint32_t color);
 void draw_bg_grid();
 void draw_rect(int x, int y, int width, int height, uint32_t color);
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
 
-
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
--- 3D-renderer/main.c
+++ 3D-renderer/main.c
@@ -113,11 +113,22 @@ void render(void) {
 
     for (int i = 0; i < N_MESH_FACES; i++) {
         triangle_t triangle = triangles_to_render[i];
-        draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
-        draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
-        draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFFFF00);
+
+        //draw_line(triangle.points[0].x, triangle.points[0].y, triangle.points[1].x, triangle.points[1].y, 0xFFFFFF00);
+        //draw_line(triangle.points[1].x, triangle.points[1].y, triangle.points[2].x, triangle.points[2].y, 0xFFFFFF00);
+        //draw_line(triangle.points[0].x, triangle.points[0].y, triangle.points[2].x, triangle.points[2].y, 0xFFFFFF00);
+
+        draw_triangle(
+            triangle.points[0].x,
+            triangle.points[0].y,
+            triangle.points[1].x,
+            triangle.points[1].y,
+            triangle.points[2].x,
+            triangle.points[2].y,
+            0xFF00FF00
+            );
+
     }
-    
     
     render_color_buffer();
     clear_color_buffer(0x00000000);
